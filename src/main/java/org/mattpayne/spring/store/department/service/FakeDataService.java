a0f368a42bbede2b3d81a789b23630ed1641cc52
Matt Payne <Payne@MattPayne.org>
2022-04-09 12:36:16 -0500

    Created one department -- now the rest.

--- src/main/java/org/mattpayne/spring/store/department/service/FakeDataService.java
+++ src/main/java/org/mattpayne/spring/store/department/service/FakeDataService.java
@@ -1,26 +1,41 @@
 package org.mattpayne.spring.store.department.service;
 
+import org.mattpayne.spring.store.department.domain.Department;
+import org.mattpayne.spring.store.department.model.DepartmentDTO;
+import org.mattpayne.spring.store.department.model.Departments;
 import org.mattpayne.spring.store.department.model.FakeDTO;
 import org.mattpayne.spring.store.department.repos.PeopleRepository;
 import org.mattpayne.spring.store.department.repos.WorkLogRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class FakeDataService {
-    private final WorkLogRepository workLogRepository;
-    private final PeopleRepository peopleRepository;
-    private final WorkLogMapper workLogMapper;
 
-    public FakeDataService(final WorkLogRepository workLogRepository,
-                          final PeopleRepository peopleRepository, final WorkLogMapper workLogMapper) {
-        this.workLogRepository = workLogRepository;
-        this.peopleRepository = peopleRepository;
-        this.workLogMapper = workLogMapper;
+    private final DepartmentService departmentService;
+
+    public FakeDataService(DepartmentService ds) {
+        this.departmentService = ds;
     }
 
     public FakeDTO createFakeDatasetOne() {
+        List<DepartmentDTO> departmentsList = fillDepartmentTable();
         FakeDTO fakeDto=new FakeDTO();
-        fakeDto.setStatus("Fake data not yet created, but it will be.");
+        fakeDto.setStatus(String.format("Created %d departments.", departmentsList.size()));
         return fakeDto;
+    }
+
+    private List<DepartmentDTO> fillDepartmentTable() {
+        DepartmentDTO departmentDTO = new DepartmentDTO();
+        departmentDTO.setDepartment(Departments.CLOTHING);
+        departmentDTO.setId(0L);
+        departmentDTO.setName(String.format("%s department", Departments.CLOTHING));
+        Long id = departmentService.create(departmentDTO);
+        departmentDTO.setId(id);
+        List<DepartmentDTO> listDepartmentDtos = new ArrayList<>();
+        listDepartmentDtos.add(departmentDTO);
+        return listDepartmentDtos;
     }
 }
