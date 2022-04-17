be8ff098b2418b3e714cc581f5dbe132e45597bd
Matt Payne <Payne@MattPayne.org>
2022-04-16 19:15:51 -0500

    Exciting!  Is this close?

--- src/main/java/org/mattpayne/spring/store/department/model/WorkInfo.java
+++ src/main/java/org/mattpayne/spring/store/department/model/WorkInfo.java
@@ -1,10 +1,12 @@
 package org.mattpayne.spring.store.department.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
 public class WorkInfo {
   private Long id;
   private String firstName, lastName, name;
--- src/main/java/org/mattpayne/spring/store/department/repos/WorkInfoRepositoryImpl.java
+++ src/main/java/org/mattpayne/spring/store/department/repos/WorkInfoRepositoryImpl.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 
 import org.mattpayne.spring.store.department.model.WorkInfo;
 
@@ -14,11 +15,28 @@ public class WorkInfoRepositoryImpl implements WorkInfoRepository {
 
     @Override
     public List<WorkInfo> getCustomWorkInfo() {
+        String query = "SELECT p.id, p.last_name, p.first_name, d.name, "
+                + "sum(((datediff('ms', '1970-01-01', wl.stop_time) - datediff('ms', '1970-01-01', wl.start_time))/1000)/(60*60)) "
+                + "as total_hours "
+                + "from "
+                + "WORK_LOG as wl, "
+                + "people as p, "
+                + "PEOPLE_WORK_IN_DEPARTMENTS as pwd, "
+                + "department  as d "
+                + "where pwd.department_id = d.id and "
+                + "p.id = pwd.people_id and "
+                + "wl.when_people_worked_id = p.id "
+                + "group by p.id "
+                + "order by total_hours desc; ";
+
         // TODO Auto-generated method stub
         List<WorkInfo> results = new ArrayList<>();
         System.out.println("***** entityMangager:");
         System.out.println(entityManager);
         System.out.println("^^^^ that is entityManager");
+        Query queryResult = entityManager.createNativeQuery(query,"getHoursReport");
+        List rl = queryResult.getResultList();
+        System.out.println(rl);
         return results;
     }
     
